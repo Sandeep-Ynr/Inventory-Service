aad3a628f532bdf61961ee691da3723d7cc3d463
ankits949 <69470342+ankits949@users.noreply.github.com>
2025-06-29 01:54:07 +0530

    make businessid integer

--- src/MilkMatrix.Core/Entities/Response/Business/BusinessMaster.cs
+++ src/MilkMatrix.Core/Entities/Response/Business/BusinessMaster.cs
@@ -2,7 +2,7 @@ namespace MilkMatrix.Core.Entities.Response.Business;
 
 public class BusinessMaster
 {
-    public string Id { get; set; } = string.Empty;
+    public int Id { get; set; }
     public string BranchAlias { get; set; } = string.Empty;
     public int? AccountBusinessId { get; set; }
     public int? UserId { get; set; }
