6d75eab4779280b201a074a27f04051c1f0a77c8
rajeevmadhu <rajeev284595@gmail.com>
2025-07-03 11:21:24 +0530

    Commented Removed

--- src/MilkMatrix.Milk/Models/Request/Geographical/VillageRequest.cs
+++ src/MilkMatrix.Milk/Models/Request/Geographical/VillageRequest.cs
@@ -5,16 +5,11 @@ namespace MilkMatrix.Milk.Models.Request.Geographical
     public class VillageRequest
     {
         public ReadActionType ActionType { get; set; } = ReadActionType.All;
-        //public int? Serial { get; set; }
         public int? VillageId { get; set; }
         public int? TehsilId { get; set; }
         public bool? IsActive { get; set; }
         public string? VillageName { get; set; }
         public bool? IsStatus { get; set; }
         public bool? IsDeleted { get; set; }
-        //public DateTime? CreatedOn { get; set; }
-        //public int? ModifyBy { get; set; }
-        //public DateTime? ModifyOn { get; set; }
-
     }
 }
