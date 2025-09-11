844c2354220f5ed8193820cfc95152d9e9fe7558
Sandeep-Ynr <sandeep327hr@gmail.com>
2025-09-11 19:43:16 +0530

    commit

--- src/MilkMatrix.Api/Models/Request/Milk/Transaction/FarmerStagingCollection/FarmerCollStgInsertRequestModel.cs
+++ src/MilkMatrix.Api/Models/Request/Milk/Transaction/FarmerStagingCollection/FarmerCollStgInsertRequestModel.cs
@@ -2,37 +2,38 @@ namespace MilkMatrix.Api.Models.Request.Milk.Transaction.FarmerStagingCollection
 {
     public class FarmerCollStgInsertRequestModel
     {
+        //public decimal RowId { get; set; }
         public DateTime DumpDate { get; set; }
         public string? DumpTime { get; set; }
-        public string Shift { get; set; } = string.Empty;
-        public string? BatchNo { get; set; }
-        public long MppId { get; set; }
-        public long BmcID { get; set; }
-        public string InsertMode { get; set; } = string.Empty;   // IMP / MANUAL
-        public string Status { get; set; } = string.Empty;       // PENDING / APPROVED
-        public long? CompanyCode { get; set; }
-        public string? ImeiNo { get; set; }
-        public bool IsValidated { get; set; }
-        public bool IsProcess { get; set; }
-        public DateTime? ProcessDate { get; set; }
-        public string? BusinessId { get; set; }
-        public List<FarmerCollectionStagingDetailModel> Details { get; set; } = new();
-
-    }
-    public class FarmerCollectionStagingDetailModel
-    {
+        public int BusinessEntityId { get; set; }
+        public string Mppcode { get; set; }
+        public string BatchNo { get; set; }
+        public long? ReferenceId { get; set; }
         public long? FarmerId { get; set; }
-        public string? Mobile { get; set; }
-        public string? FarmerCode { get; set; }
+        public string? FName { get; set; }
+        public string Shift { get; set; }
+        public string Type { get; set; }
+        public decimal WeightLiter { get; set; }
         public decimal? Fat { get; set; }
         public decimal? Snf { get; set; }
         public decimal? LR { get; set; }
-        public decimal? WeightLiter { get; set; }
-        public string? Type { get; set; }          // Cow / Buffalo
-        public decimal? Rptl { get; set; } // RTPL
+        public int? Can { get; set; }
+        public decimal? Rtpl { get; set; }
         public decimal? TotalAmount { get; set; }
         public int? SampleId { get; set; }
-        public int? Can { get; set; }
-        public long? ReferenceId { get; set; }
+        public string? IsQltyAuto { get; set; }
+        public string? IsQtyAuto { get; set; }
+        public string InsertMode { get; set; }
+        public string? IMEI_No { get; set; }
+        public bool IsValidated { get; set; }
+        public bool IsProcess { get; set; }
+        public DateTime? ProcessDate { get; set; }
+        public bool? IsStatus { get; set; }
+        //public bool? IsDeleted { get; set; }
+        //public int? CreatedBy { get; set; }
+        //public DateTime? CreatedOn { get; set; }
+        //public int? DeletedBy { get; set; }
+        //public DateTime? DeletedOn { get; set; }
     }
+   
 }
